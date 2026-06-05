b4d7e0744e3e1fb1013b86a8c498086721afdc94
cryptocreeper94 <jason@livfitoken.com>
2026-06-05 11:55:51 -0500

    chore: version bump and capacitor setup

---
+++ android/app/src/main/java/com/darkwave/trustshield/MainActivity.java
@@ -0,0 +1,5 @@
+package com.darkwave.trustshield;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.darkwave.trustshield',
+  appName: 'TrustShield',
+  webDir: 'dist/client'
+};
+
+export default config;
